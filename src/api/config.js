339059f9e094015db54fb8ae9ca05e6d1aecb245
Carlos Millan <atlantis9@hotmail.com>
2017-06-24 13:51:48 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,9 @@
+export default {
+  appName: 'Platzimusic',
+  apiKey: 'ba3b25ca3ed8ad971f46f2a32ad183b6',
+  secret: 'a0d3c7704eb61156add52761992a08f3',
+  registedTo: 'andresito_30'
+}
+
+
+
---
+++ src/api/index.js
@@ -0,0 +1,14 @@
+import config from './config.js'
+
+const { apiKey } = config
+
+const URL = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=${apiKey}&format=json`
+
+function getArtists (country) {
+  const url = URL.replace(':country', country)
+  return fetch(url)
+    .then(res => res.json())
+    .then(json => json.topartists.artist)
+}
+
+export default getArtists
